2c09326952e1be4c43dee28b6ad6e0b8467feec1
lorenzorogers <129799525+lorenzorogers@users.noreply.github.com>
2025-03-18 19:21:50 -0600

    fix: NavTextButton borders

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://lorenzorogers.com"
+  site: "https://lorenzorogers.com",
 });
